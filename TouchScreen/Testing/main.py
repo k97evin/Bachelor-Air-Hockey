58b2c82c4f36cc020ec49f0d9bacff6a0ee9773b
Freddekanon <fredrikericsson95@hotmail.com>
2021-05-26 13:34:43 +0200

    Made sliders for cameracalibration

--- TouchScreen/Testing/main.py
+++ TouchScreen/Testing/main.py
@@ -3,12 +3,16 @@ import threading
 import time
 from kivy.app import App
 
+import cv2
+import numpy as np
+
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 
 from kivy.properties import NumericProperty
 
@@ -27,8 +31,8 @@ Config.write()
 
 
 
-class Widget(Widget):
-    score = NumericProperty(0)
+#class Widget(Widget):
+#    score = NumericProperty(0)
 
 
 class MainWindow(Screen):
@@ -41,7 +45,33 @@ class LiveCalculationWindow(Screen):
     pass
 
 class CameraWindow(Screen):
-    pass
+
+    font_size = NumericProperty(15)
+
+    def slide_hue_min(self, *args):
+        print(args[1])
+        self.slide_text_hue_min.text = "Hue min: 0" + str(int(args[1]))
+        #self.slide_text.font_size = str(int(args[1]) + 20)
+
+    def slide_hue_max(self, *args):
+        print(args[1])
+        self.slide_text_hue_max.text = "Hue max: 0" + str(int(args[1]))
+
+    def slide_sat_min(self, *args):
+        print(args[1])
+        self.slide_text_sat_min.text = "Sat min: 0" + str(int(args[1]))
+
+    def slide_sat_max(self, *args):
+        print(args[1])
+        self.slide_text_sat_max.text = "Sat max: 0" + str(int(args[1]))
+
+    def slide_val_min(self, *args):
+        print(args[1])
+        self.slide_text_val_min.text = "Val min: 0" + str(int(args[1]))
+
+    def slide_val_max(self, *args):
+        print(args[1])
+        self.slide_text_val_max.text = "Val max: 0" + str(int(args[1]))
 
 class CalibrationWindow(Screen):
     pass
@@ -50,6 +80,7 @@ class SettingsWindow(Screen):
     pass
 
 class WindowManager(ScreenManager):
+
     pass
 
 kv = Builder.load_file("my.kv")
@@ -69,6 +100,7 @@ sm.current = "Main"
 class MyApp(App):
     def build(self):
         return sm
+
 
 
 def run():
