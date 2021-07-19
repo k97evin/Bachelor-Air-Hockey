ae19f115f85652c7ab858e4a092a01c32ff06486
BO21E_18 <k97evin@hotmail.no>
2021-07-19 17:11:01 +0200

    Fixed all arduino files

--- arduino/TestPrograms/increasingSpeed/increasingSpeed.ino
+++ arduino/TestPrograms/increasingSpeed/increasingSpeed.ino
@@ -1,3 +1,11 @@
+/*
+
+This script increases the speed of the stepper motors each time it has finished a path.
+Uncomment the desired path below to change path.
+
+*/
+
+
 #include "FastAccelStepper.h"
 // With the current stepper pins selected the FastAccelStepper library needs to be changed. 
 // Open the file ...\Arduino\libraries\FastAccelStepper\src\AVRStepperPins.h and change line 27 to: #define FAS_TIMER_MODULE 5
@@ -22,8 +30,8 @@
 
 
 //Variables
-int motorSpeed = 100; //maximum steps per second (about 3rps / at 16 microsteps)  //max 8000
-int motorAccel = 31000; //steps/second/second to accelerate  //max 31000
+int motorSpeed = 200; // mm/s
+int motorAccel = 31000; // steps/s^2 to accelerate  //max 31000
 int SPR = 400; // Steps Per Rev
 
 int stepperL_vel = 0 ;
@@ -34,6 +42,7 @@ int pos_x = 0;
 int pos_y = 0;
 
 float radius = 25.205;
+
 
 // --- DIFFERENT PATHS --- //
 
@@ -49,21 +58,19 @@ float radius = 25.205;
 //Cross and square path
 float pointsArray[][2] = {{100,100},{600,600},{100,600},{600,100},{100,100},{100,600},{600,600},{600,100}};
 
+
+
 const int ANT_POINTS = sizeof(pointsArray)/8;
 int pointNum = 0;
 
 int time = 0;
 
+//Defining stepper motors
 FastAccelStepperEngine engine = FastAccelStepperEngine();
 FastAccelStepper *stepperL = NULL;
 FastAccelStepper *stepperR = NULL;
 
 
-
-//void MoveToPosition(float x, float y){
-// stepperL->moveTo(thetaL(x,y,true));
-// stepperR->moveTo(thetaR(x,y,true));
-//}
 
 void setup() {
   // Pins and Serial
@@ -74,10 +81,7 @@ void setup() {
   pinMode(endSwitch_U, INPUT);
   pinMode(endSwitch_D, INPUT);
   pinMode(friBryter,INPUT);
-  
-  Serial.println("Begynner");
 
-  //TCCR5B = TCCR5B & B11111000 | B00000001;
   
   // --- Connecting to steppers --- //
   Connect_to_steppers();
@@ -106,17 +110,9 @@ void loop(){
       pointNum = 0;
       motorSpeed += 100;
       Serial.println("Speed:" + String(motorSpeed) +"mm/s  stepperL:" + String(stepperL_vel) + "steps/s   stepperR:" + String(stepperR_vel)  + "step/s");
-
-      //Serial.println("Tid: " + String(millis()-time));
-      //delay(10000);
     }
     else{
       pointNum ++;
-      //if(motorSpeed == 3000){
-       // motorSpeed = 4500;
-      //}
-      //int speedStepsL = stepperL->getSpeedInMilliHz();
-      //int speedStepsR = stepperR->getSpeedInMilliHz();
       delayMicroseconds(500);
     }
   }
