aac0fa5dcbf93d2a02ab852ecead5188ea8b9bc6
k97evin <51542021+k97evin@users.noreply.github.com>
2021-07-23 11:39:30 +0200

    Added arduino test files

---
+++ arduino/TestPrograms/basicMotorControl/basicMotorControl.ino
@@ -0,0 +1,96 @@
+#include "FastAccelStepper.h"
+
+// With the current stepper pins selected the FastAccelStepper library needs to be changed. 
+// Open the file ...\Arduino\libraries\FastAccelStepper\src\AVRStepperPins.h and change line 27 to: #define FAS_TIMER_MODULE 5
+
+
+//Stepper pins
+#define dirPin_L 27
+#define dirPin_R 28
+#define enablePin 29
+#define stepPin_L 46  //13
+#define stepPin_R 44  //12
+
+//End switches
+#define endSwitch_L 39  
+#define endSwitch_R 41
+#define endSwitch_U 35
+#define endSwitch_D 37
+
+#define friBryter 43
+
+
+void setup() {
+  // Pins and Serial
+  Serial.begin(9600);
+  pinMode(enablePin, OUTPUT);
+  pinMode(endSwitch_L,INPUT);
+  pinMode(endSwitch_R,INPUT);
+  pinMode(endSwitch_U, INPUT);
+  pinMode(endSwitch_D, INPUT);
+  pinMode(friBryter,INPUT);
+  
+  
+  // --- Connecting to steppers --- //
+  Serial.println("Connectiong to stepper left");
+  delay(100);
+  engine.init();
+
+  stepperL = engine.stepperConnectToPin(stepPin_L);
+  if (stepperL) {
+    stepperL->setDirectionPin(dirPin_L);
+    stepperL->setEnablePin(enablePin);
+
+    Serial.println("stepper left found");
+    Serial.println(stepperL->getCurrentPosition());
+  }
+  else{
+    Serial.println("Error: Cant connect to stepper left");
+  }
+
+
+  Serial.println("Connectiong to stepper right");
+  delay(100);
+
+  stepperR = engine.stepperConnectToPin(stepPin_R);
+  if (stepperR) {
+    stepperR->setDirectionPin(dirPin_R);
+    stepperR->setEnablePin(enablePin);
+    //stepperR->setAutoEnable(true);
+
+    Serial.println("stepper right found");
+
+    Serial.println(stepperR->getCurrentPosition());
+  }
+
+  else{
+    Serial.println("Error: Cant connect to stepper right");
+  }
+
+
+  // --- activating limit switches --- //
+  digitalWrite(enablePin, LOW);
+  delay(1000);
+
+  // --- stepper program --- //
+  stepperL->setSpeedInHz(100); //steps/sec
+  stepperL->setAcceleration(100); //steps/sec^2 
+  stepperR->setSpeedInHz(100);
+  stepperR->setAcceleration(100);   
+
+  //moving left stepper 100 steps (1/4 round)
+  Serial.println("Moving left stepper");
+  stepperL->move(100); 
+
+  delay(3000);
+
+  //moving right stepper 100 steps (1/4 round)
+  Serial.println("Moving right stepper");
+  stepperR->move(100); 
+}
+
+
+
+void loop(){
+
+}
---
+++ arduino/TestPrograms/stepperTime/stepperTime.ino
@@ -0,0 +1,122 @@
+#include "FastAccelStepper.h"
+// With the current stepper pins selected the FastAccelStepper library needs to be changed. 
+// Open the file ...\Arduino\libraries\FastAccelStepper\src\AVRStepperPins.h and change line 27 to: #define FAS_TIMER_MODULE 5
+
+//Stepper pins
+#define dirPin_L 27
+#define dirPin_R 28
+#define enablePin 29
+#define stepPin_L 46  //13
+#define stepPin_R 44  //12
+
+//End switches
+#define endSwitch_L 39  
+#define endSwitch_R 41
+#define endSwitch_U 35
+#define endSwitch_D 37
+
+#define friBryter 43
+
+
+
+
+
+//Variables
+int motorSpeed = 2300; //maximum steps per second (about 3rps / at 16 microsteps)  //max 8000
+int motorAccel = 31000; //steps/second/second to accelerate  //max 31000
+int SPR = 400; // Steps Per Rev
+
+int stepperL_vel = 0 ;
+int stepperR_vel = 0;
+
+int i = 0;
+int pos_x = 0;
+int pos_y = 0;
+
+float radius = 25.205;
+
+// --- DIFFERENT PATHS --- //
+
+//Only stepper right
+//float pointsArray[][2] = {{150,150},{550,550}};
+
+//Only stepper left
+//float pointsArray[][2] = {{150,550},{550,150}};
+
+//Square path
+//float pointsArray[][2] = {{150,150},{150,550},{550,550},{550,150},{150,150}};
+
+//Cross and square path
+float pointsArray[][2] = {{100,100},{600,600},{100,600},{600,100},{100,100},{100,600},{600,600},{600,100},{100,100}};
+
+const int ANT_POINTS = sizeof(pointsArray)/8;
+int pointNum = 0;
+
+int time = 0;
+bool done = false;
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepperL = NULL;
+FastAccelStepper *stepperR = NULL;
+
+
+
+//void MoveToPosition(float x, float y){
+// stepperL->moveTo(thetaL(x,y,true));
+// stepperR->moveTo(thetaR(x,y,true));
+//}
+
+void setup() {
+  // Pins and Serial
+  Serial.begin(9600);
+  pinMode(enablePin, OUTPUT);
+  pinMode(endSwitch_L,INPUT);
+  pinMode(endSwitch_R,INPUT);
+  pinMode(endSwitch_U, INPUT);
+  pinMode(endSwitch_D, INPUT);
+  pinMode(friBryter,INPUT);
+  
+  Serial.println("Begynner");
+
+  //TCCR5B = TCCR5B & B11111000 | B00000001;
+  
+  // --- Connecting to steppers --- //
+  Connect_to_steppers();
+
+  // --- stepper start values --- //
+  stepperL->setSpeedInHz(motorSpeed);
+  stepperL->setAcceleration(motorAccel);    
+  stepperR->setSpeedInHz(motorSpeed);
+  stepperR->setAcceleration(motorAccel);   
+
+  // --- zeroing --- //
+  digitalWrite(enablePin, LOW);
+  goto_zero();
+
+  stepperL->setSpeedInHz(motorSpeed);
+  stepperR->setSpeedInHz(motorSpeed);
+
+  time = millis();
+}
+
+void loop(){
+
+  if (done == false){
+    MoveToPosition(pointsArray[pointNum][0],pointsArray[pointNum][1],motorSpeed);
+    delayMicroseconds(500);
+    if(stepperL->targetPos() == stepperL->getCurrentPosition() && stepperR->targetPos() == stepperR->getCurrentPosition()){
+      if(pointNum == ANT_POINTS-1){
+        Serial.println("Time used:" + String((millis()-time)/1000.0) + "s");
+        //Serial.println("Tid: " + String(millis()-time));
+        delay(10000);
+        done = true;
+      }
+      else{
+        pointNum ++;
+        delayMicroseconds(500);
+      }
+    }
+  }
+
+}
+
+
